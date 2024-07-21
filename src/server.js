8701eafa812b57ee842a3e64e0c9956a42bb3083
sallyhsieh <sallyhsieh@liontrave.com>
2024-07-21 22:46:32 +0800

    split

--- src/server.js
+++ src/server.js
@@ -1,66 +1,69 @@
 require("dotenv").config();
 
 const express = require("express");
-const line = require("@line/bot-sdk");
-const openAIChat = require("./utils/openai");
+const api = require("./api");
+// const line = require("@line/bot-sdk");
+// const openAIChat = require("./utils/openai");
 
-// create LINE SDK config from env variables
-const config = {
-  channelSecret: process.env.CHANNEL_SECRET,
-};
+// // create LINE SDK config from env variables
+// const config = {
+//   channelSecret: process.env.CHANNEL_SECRET,
+// };
 
-// create LINE SDK client
-const client = new line.messagingApi.MessagingApiClient({
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-});
+// // create LINE SDK client
+// const client = new line.messagingApi.MessagingApiClient({
+//   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+// });
 
 const app = express();
 
-app.post("/callback", line.middleware(config), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
-});
+app.use("/", api);
 
-// event handler
-async function handleEvent(event) {
-  if (event.type !== "message" || event.message.type !== "text") {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
+// app.post("/callback", line.middleware(config), (req, res) => {
+//   Promise.all(req.body.events.map(handleEvent))
+//     .then((result) => res.json(result))
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).end();
+//     });
+// });
 
-  const response = await openAIChat(event.message.text);
+// // event handler
+// async function handleEvent(event) {
+//   if (event.type !== "message" || event.message.type !== "text") {
+//     // ignore non-text-message event
+//     return Promise.resolve(null);
+//   }
 
-  // create an echoing text message
-  const echo = { type: "text", text: response };
+//   const response = await openAIChat(event.message.text);
 
-  // use reply API
-  return client.replyMessage({
-    replyToken: event.replyToken,
-    messages: [echo],
-  });
-}
+//   // create an echoing text message
+//   const echo = { type: "text", text: response };
 
-const jsonParser = express.json();
+//   // use reply API
+//   return client.replyMessage({
+//     replyToken: event.replyToken,
+//     messages: [echo],
+//   });
+// }
 
-app.get("/test", jsonParser, async (req, res) => {
-  const { message } = req.body;
+// const jsonParser = express.json();
 
-  if (!message) {
-    res.send({ error: "No messages provided" });
-    return;
-  }
+// app.get("/test", jsonParser, async (req, res) => {
+//   const { message } = req.body;
 
-  try {
-    const response = await openAIChat(message);
-    res.send(response);
-  } catch (error) {
-    res.status(500).send({ error: error.name });
-  }
-});
+//   if (!message) {
+//     res.send({ error: "No messages provided" });
+//     return;
+//   }
+
+//   try {
+//     const response = await openAIChat(message);
+//     res.send(response);
+//   } catch (error) {
+//     res.status(500).send({ error: error.name });
+//   }
+// });
 
 app.get("*", (req, res) => {
   res.status(404).json({ error: "Page did not exist" });
